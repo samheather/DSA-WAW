6be649161800353575c36886d49dbd5d0d6e835c
Adam Taylor <at895@york.ac.uk>
2013-11-25 09:09:31 +0000

    Controls update

--- Game/src/Controls.java
+++ Game/src/Controls.java
@@ -4,8 +4,6 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
-import org.newdawn.slick.UnicodeFont;
-import org.newdawn.slick.font.effects.ColorEffect;
 import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.GameContainer;
 
