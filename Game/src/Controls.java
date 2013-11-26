c19b06405f2b95ec4ec3ff6eadad0ad12cf66420
Adam Taylor <at895@york.ac.uk>
2013-11-26 21:10:36 +0000

    Added turn left and right text fields

--- Game/src/Controls.java
+++ Game/src/Controls.java
@@ -13,8 +13,12 @@ public class Controls {
 	private TrueTypeFont font;
 	private TextField headingControlTB;
 	private TextField altControlTB;
+	private TextField turnRightTB;
+	private TextField turnLeftTB;
 	private boolean headingHasFocus; // Is the text box currently selected?
 	private boolean altHasFocus; // Is the text box currently selected?
+	private boolean turnLeftHasFocus;
+	private boolean turnRightHasFocus;
 	private boolean heading_cleared_this_focus; // Has the focus been reset?
 	private boolean alt_cleared_this_focus; // Has the focus been reset?
 	private Flight flight;
@@ -30,8 +34,12 @@ public class Controls {
 		font = new TrueTypeFont(awtFont, false);
 		this.headingControlTB = new TextField(gc, font, 0, 30, 100, 25);
 		this.altControlTB = new TextField(gc, font, 0, 85, 100, 25);
+		this.turnLeftTB = new TextField(gc,font,0,140,100,25);
+		this.turnRightTB = new TextField(gc,font,0,195,100,25);
 		this.headingHasFocus = false;
 		this.altHasFocus = false;
+		this.turnLeftHasFocus = false;
+		this.turnRightHasFocus = false;
 		this.heading_cleared_this_focus = false;
 	}
 
@@ -48,6 +56,7 @@ public class Controls {
 	public boolean altHasFocus() {
 		return this.altHasFocus;
 	}
+	
 
 	public TextField getAltControlTB() {
 		return altControlTB;
@@ -56,11 +65,15 @@ public class Controls {
 	public void clear_all() {
 		this.headingControlTB.setAcceptingInput(false);
 		this.altControlTB.setAcceptingInput(false);
+		this.turnLeftTB.setAcceptingInput(false);
+		this.turnRightTB.setAcceptingInput(false);
 	}
 
 	public void allow_all() {
 		this.headingControlTB.setAcceptingInput(true);
 		this.altControlTB.setAcceptingInput(true);
+		this.turnLeftTB.setAcceptingInput(true);
+		this.turnRightTB.setAcceptingInput(true);
 	}
 
 	// UPDATE AND RENDER
@@ -110,6 +123,28 @@ public class Controls {
 				this.flight.setTarget_altitude(Double.valueOf(this.text));
 			}
 		}
+		this.turnLeftHasFocus = this.turnLeftTB.hasFocus();
+		if (this.turnLeftHasFocus) {
+			if (input.isKeyDown(Input.KEY_ENTER)) {
+				this.text = this.turnLeftTB.getText();
+				this.text = this.text.replaceAll("\\D+", "");
+				if (!this.text.isEmpty()) {
+					this.flight.turn_flight_left(Integer.valueOf(this.text));
+				}
+				this.turnLeftTB.setFocus(false);
+			}
+		}
+		this.turnRightHasFocus = this.turnRightTB.hasFocus();
+		if (this.turnRightHasFocus) {
+			if (input.isKeyDown(Input.KEY_ENTER)) {
+				this.text = this.turnRightTB.getText();
+				this.text = this.text.replaceAll("\\D+", "");
+				if (!this.text.isEmpty()) {
+					this.flight.turn_flight_right(Integer.valueOf(this.text));
+				}
+				this.turnRightTB.setFocus(false);
+			}
+		}
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
@@ -118,5 +153,9 @@ public class Controls {
 		this.headingControlTB.render(gc, g);
 		g.drawString("Altitude:", 0, 60);
 		this.altControlTB.render(gc, g);
+		g.drawString("Turn Left:",0,110);
+		this.turnLeftTB.render(gc,g);
+		g.drawString("Turn Right:",0,160);
+		this.turnRightTB.render(gc,g);
 	}
 }
