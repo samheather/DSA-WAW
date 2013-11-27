95f745a8f6aefc8547fccdd5569f9a793b388ff0
Adam Taylor <at895@york.ac.uk>
2013-11-27 10:42:54 +0000

    formatting

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -2,29 +2,28 @@ import java.util.ArrayList;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
+public class Game extends StateBasedGame {
 
-public class Game extends StateBasedGame  {
-	
 	public static final String NAME = "ATC Game - Team WAW";
 	public static final int MENU = 0;
 	public static final int PLAY = 1;
 	public static final int MAXIMUM_WIDTH = 1200;
 	public static final int MAXIMUM_HEIGHT = 600;
-	
+
 	public Game(String NAME) {
 		super(NAME);
 		this.addState(new Menu(MENU));
 		this.addState(new Play(PLAY));
 		this.enterState(MENU);
 	}
+
 	public void initStatesList(GameContainer gc) throws SlickException {
-		
-		//this.getState(MENU).init(gc, this);
-		//this.getState(PLAY).init(gc, this);
-		
-		
+
+		// this.getState(MENU).init(gc, this);
+		// this.getState(PLAY).init(gc, this);
+
 	}
-	
+
 	public static void main(String[] args) {
 		AppGameContainer appgc;
 		try {
@@ -33,7 +32,7 @@ public class Game extends StateBasedGame  {
 			appgc.setTargetFrameRate(60);
 			appgc.setIcon("res/icon.png");
 			appgc.start();
-		}catch(SlickException e){
+		} catch (SlickException e) {
 			e.printStackTrace();
 		}
 	}
