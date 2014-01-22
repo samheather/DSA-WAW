ced8a920a26b3e1e10cb9fe31a655c6823466ba5
Liviu Pirvan <lp752@york.ac.uk>
2014-01-22 01:10:46 +0000

    added hovers

--- Game/src/states/PauseState.java
+++ Game/src/states/PauseState.java
@@ -12,7 +12,7 @@ public class PauseState extends BasicGameState {
 	public static TrueTypeFont font;
 	private int pageNumber;
 
-	private Image pauseBackgroundPage1,pauseBackgroundPage2, backButton, nextPageButton, previousPageButton, menuButton, quitButton;
+	private Image pauseBackgroundPage1,pauseBackgroundPage2, backButtonHover, nextPageButton, previousPageButton, menuButton, quitButton, nextPageButtonHover, previousPageButtonHover, quitButtonHover, backHover;
 	
 	
 	public PauseState(int state) {
@@ -25,11 +25,15 @@ public class PauseState extends BasicGameState {
 			
 			pauseBackgroundPage1 = new Image("res/menu_graphics/pause_screen.jpg");
 			pauseBackgroundPage2 = new Image("res/menu_graphics/controls2.jpg");
-			backButton = new Image("res/menu_graphics/back.png");
+			backButtonHover = new Image("res/menu_graphics/back.png");
+			backHover = new Image("res/menu_graphics/back_hover.png");
 			nextPageButton = new Image("res/menu_graphics/next page.png");
+			nextPageButtonHover = new Image("res/menu_graphics/next page_hover.png");
 			previousPageButton = new Image("res/menu_graphics/previous page.png");
+			previousPageButtonHover = new Image("res/menu_graphics/previous hover.png");
 			menuButton = new Image("res/menu_graphics/menu_button.png");
 			quitButton = new Image("res/menu_graphics/quit_button.png");
+			quitButtonHover = new Image("res/menu_graphics/quit_hover.png");
 			
 			InputStream inputStream = ResourceLoader.getResourceAsStream("res/blue_highway_font/bluehigh.ttf");
 			Font awtFont= Font.createFont(Font.TRUETYPE_FONT, inputStream);
@@ -45,23 +49,43 @@ public class PauseState extends BasicGameState {
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		
+		int posX = Mouse.getX();
+		int posY = Mouse.getY();
+		
+		posY = 600 - posY;
+		
 		g.setFont(font);
-		if (pageNumber == 1) {
-			pauseBackgroundPage1.draw(0,0);
-			backButton.draw(20,20);
-			nextPageButton.draw(1030,280);
-			//menuButton.draw(1050, 20);
-			quitButton.draw(1150,550);
-			g.setColor(Color.white);
+		
+		pauseBackgroundPage1.draw(0,0);
+		
+		if((posX > 20 && posX < 40) && (posY > 20 && posY < 40)) {
+			backHover.draw(20,20);
+		}else{
+			backButtonHover.draw(20,20);
 		}
 		
-		else if (pageNumber == 2){
-			pauseBackgroundPage2.draw(0,0);
-			backButton.draw(20,20);
-			previousPageButton.draw(30,280);
-			//menuButton.draw(1050, 20);
+		
+		if((posX > 1150 && posX < 1170) && (posY > 550 && posY < 580)) {
+			quitButtonHover.draw(1150,550);
+		}else{
 			quitButton.draw(1150,550);
+		}
+		
+		if (pageNumber == 1) {
 			
+			if((posX > 1020 && posX < 1150) && (posY > 270 && posY < 330)) {
+				nextPageButtonHover.draw(1030,280);
+			} else {
+				nextPageButton.draw(1030,280);
+			}
+			g.setColor(Color.white);
+		}else if (pageNumber == 2){
+						
+			if((posX > 50 && posX < 240) && (posY > 280 && posY < 320)) {
+				previousPageButtonHover.draw(30,280);
+			} else {
+				previousPageButton.draw(30,280);				
+			}			
 		}
 	
 	}
@@ -82,23 +106,21 @@ public class PauseState extends BasicGameState {
 		
 		
 		if((posX > 20 && posX < 40) && (posY > 20 && posY < 40)) {
+						
 			if(Mouse.isButtonDown(0)) {
 				pageNumber = 1;
 				sbg.enterState(1);
 			}
 			
 		}
-		else {
-		}
 		
 		
 		if((posX > 1150 && posX < 1170) && (posY > 550 && posY < 580)) {
+			
 			if(Mouse.isButtonDown(0)) {
 				System.exit(0);
 			}
 			
-		}
-		else {
 		}
 		
 		if (pageNumber == 1){
