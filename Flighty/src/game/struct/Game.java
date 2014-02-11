012073160184b3eed1ebe81efce827fc39cb19a1
L Pirvan <lp752@pc952s.cs.york.ac.uk>
2014-02-11 15:30:10 +0000

    fixed taking off without pressing

--- Flighty/src/game/struct/Game.java
+++ Flighty/src/game/struct/Game.java
@@ -474,7 +474,7 @@ public class Game {
 					if(!this.manualPlanes.contains(this.currentPlane)) {
 						this.manualPlanes.add(this.currentPlane);
 					}
-					
+			
 					this.currentPlane.decrementBearing();
 				}
 
@@ -555,9 +555,10 @@ public class Game {
 				//if(plane.getVelocity()==0) {
 				//	this.manualPlanes.add(plane);
 				//}
+				// Press 't' to take off
 				if(plane.equals(this.currentPlane)) { //<- this is what I'm talking about on google, you can only take of if you press an arrow first or right click
 					if(plane.getVelocity()==0) {
-						if(gameContainer.getInput().isKeyPressed(Input.KEY_T)) {
+						if(gameContainer.getInput().isKeyDown(Input.KEY_T)) {
 							
 							plane.setVelocity(7000);
 							this.manualPlanes.remove(plane);
