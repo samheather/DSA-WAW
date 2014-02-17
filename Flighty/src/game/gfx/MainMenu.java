8be89e5e2a20ee56a390744bf5c6598342dad0f8
hk3380 <lp752@york.ac.uk>
2014-02-17 23:55:55 +0000

    fixes #10, added comments and put the setters/getters at the end

--- Flighty/src/game/gfx/MainMenu.java
+++ Flighty/src/game/gfx/MainMenu.java
@@ -24,6 +24,236 @@ public class MainMenu extends GenericWindow {
 	/** The waypoint icon */
 	private Image waypointIcon;
 	
+	/**
+	 * Monitors the mouse position and state
+	 * <p>
+	 * This causes options to be highlighted etc. when hovered over.
+	 * </p>
+	 * <p>
+	 * Also handles item selection (through clicking)
+	 * </p>
+	 * 
+	 * @param gameContainer	the game container holding this state
+	 * @param game			the game running this state
+	 */
+	private void checkForSelection(GameContainer gameContainer,
+				StateBasedGame game)
+		{
+			int x 					= gameContainer.getInput().getMouseX();
+			int y 					= gameContainer.getInput().getMouseY();
+			boolean clicked 		= gameContainer.getInput().isMousePressed(0);
+			
+			String startGameText 	= "Start Game";
+			String creditsText 		= "Credits";
+			String controlsText 	= "Controls";
+			String exitText 		= "Exit";
+			
+			int startGameWidth 		= this.font.getWidth(startGameText);
+			int creditsWidth 		= this.font.getWidth(creditsText);
+			int controlsWidth 		= this.font.getWidth(controlsText);
+			int exitWidth 			= this.font.getWidth(exitText);
+			
+			int textHeight 			= this.font.getHeight();
+			
+			Color startGameColor 	= Color.orange;
+			Color creditsColor 		= Color.orange;
+			Color controlsColor 	= Color.orange;
+			Color exitColor 		= Color.orange;
+			
+			// Start Game
+			if((x >= (gameContainer.getWidth() / 3) + 30 - 25)
+					&& (y >= (gameContainer.getHeight() / 3) + 50 - 25)
+					&& (x <= (gameContainer.getWidth() / 3) + 30
+					+ startGameWidth + 25)
+					&& (y <= (gameContainer.getHeight() / 3) + 50
+					+ textHeight + 25))
+			{
+				if(clicked)
+				{
+					// Change game state
+					game.enterState(WindowManager.LEVEL_SELECT_STATE);
+				}
+				else
+				{
+					// Change hover text and add waypoint next to text
+					startGameColor = Color.white;
+					this.waypointIcon.draw((gameContainer.getWidth() / 3)
+							+ startGameWidth + 30, (gameContainer.getHeight() / 3) + 53);
+				}
+			}
+			else
+			{
+				startGameColor = Color.orange;
+			}
+			
+			// Draw the Start Game text with a shadow
+			this.drawShadowedText(this.font, (gameContainer.getWidth() / 3)
+					+ 30, (gameContainer.getHeight() / 3) + 50,
+					startGameText, startGameColor);
+			
+			
+			// Credits
+			if((x >= (gameContainer.getWidth() / 3) + 30 - 25)
+					&& (y >= (gameContainer.getHeight() / 3) + 50 
+					+ (this.font.getHeight() * 4) - 25)
+					&& (x <= (gameContainer.getWidth() / 3) + 30
+					+ creditsWidth + 25)
+					&& (y <= (gameContainer.getHeight() / 3) + 50
+					+ (this.font.getHeight() * 4) + textHeight + 25))
+			{
+				if(clicked)
+				{
+					// Change game state
+					game.enterState(WindowManager.CREDITS_STATE);
+				}
+				else
+				{
+					creditsColor = Color.white;
+					
+					// Change hover text and add waypoint next to text
+					this.waypointIcon.draw((gameContainer.getWidth() / 3)
+							+ 30 + creditsWidth, (gameContainer.getHeight() / 3) 
+							+ 54 + (this.font.getHeight() * 4));
+				}
+			}
+			else
+			{
+				creditsColor = Color.orange;
+			}
+			
+			// Draw the Credits text with a shadow
+			this.drawShadowedText(this.font, (gameContainer.getWidth() / 3)
+					+ 30, (gameContainer.getHeight() / 3) + 50
+					+ (this.font.getHeight() * 4), creditsText, creditsColor);
+			
+			
+			// Controls
+					if((x >= (gameContainer.getWidth() / 3) + 30 - 25)
+							&& (y >= (gameContainer.getHeight() / 3) + 150 
+							+ (this.font.getHeight() * 4))
+							&& (x <= (gameContainer.getWidth() / 3) + 30
+							+ creditsWidth + 25)
+							&& (y <= (gameContainer.getHeight() / 3) + 160
+							+ (this.font.getHeight() * 4) + textHeight + 25))
+					{
+						if(clicked)
+						{
+							// Change game state
+							game.enterState(WindowManager.CONTROLS_STATE);
+						}
+						else
+						{
+							// Change hover text and add waypoint next to text
+							controlsColor = Color.white;
+							this.waypointIcon.draw((gameContainer.getWidth() / 3)
+									+ controlsWidth + 30, (gameContainer.getHeight() / 3) + 153
+									+ (this.font.getHeight() * 4));
+						}
+					}
+					else
+					{
+						controlsColor = Color.orange;
+					}
+			
+			// Draw the Controls text with a shadow
+			this.drawShadowedText(this.font, (gameContainer.getWidth() / 3)
+						+ 30, (gameContainer.getHeight() / 3) + 150
+						+ (this.font.getHeight() * 4), controlsText, controlsColor);
+					
+			
+			
+			// Exit
+			if((x >= (50 - 25))
+					&& (y >= (gameContainer.getHeight() - 50 - 25))
+					&& (x <= (50 - 25) + exitWidth + 25)
+					&& (y <= (gameContainer.getHeight() - 50 + textHeight + 25)))
+			{
+				if(clicked)
+				{
+					System.exit(0);
+				}
+				else
+				{
+					exitColor = Color.white;
+				}
+			}
+			else
+			{
+				// Change colour on hover
+				exitColor = Color.orange;
+			}
+			
+			// Draw the Exit text with a shadow
+			this.drawShadowedText(this.font, 50, gameContainer.getHeight()
+					- 50, exitText, exitColor);
+		}
+		
+		// Overrides
+		/**
+		 * Initialises the state
+		 * 
+		 * @param gameContainer	the game container holding this state
+		 * @param game			the game running this state
+		 */
+		@Override
+		public void init(GameContainer gameContainer,
+				StateBasedGame game) throws SlickException {
+			super.init(gameContainer, game);
+			
+			InputStream backgroundStream = this.getClass()
+					.getResourceAsStream(
+							"/resources/backgrounds/CloudBackground.png");
+			InputStream planeStream = this.getClass()
+					.getResourceAsStream("/resources/planes/TitlePlane.png");
+			InputStream waypointStream = this.getClass()
+					.getResourceAsStream("/resources/waypoints/WaypointRed.png");
+			
+			this.backgroundImage = new Image(backgroundStream,
+					"Background Image", false);
+			this.planeIcon = new Image(planeStream,
+					"Plane Icon", false);		
+			this.waypointIcon = new Image(waypointStream,
+					"Waypoint Icon", false);
+		}
+		
+		/**
+		 * Renders the state
+		 * 
+		 * @param gameContainer	the game container holding this state
+		 * @param game			the game running this state
+		 * @param graphics		the graphics container to display content in
+		 */
+		@Override
+		public void render(GameContainer gameContainer,
+				StateBasedGame game, Graphics graphics) throws SlickException {
+			super.render(gameContainer, game, graphics);
+			
+			// Draw background
+			this.backgroundImage.draw((float) (0 - ((WindowManager) game)
+					.getSkyProgress()), 0, gameContainer.getWidth() * 2,
+					gameContainer.getHeight());
+			
+			// Draw main title
+			int mainTitleXPos = (gameContainer.getWidth() / 2)
+					- (this.titleFont.getWidth("World of") / 2);
+			
+			int subtitleXPos = (gameContainer.getWidth() / 2)
+					- (this.titleFont.getWidth("Flighty") / 2);
+			
+			this.drawShadowedText(this.titleFont, mainTitleXPos, 50,
+					"World of", Color.orange);
+			this.drawShadowedText(this.titleFont, subtitleXPos, 125,
+					"Flighty", Color.orange);
+			
+			// Draw other text
+			this.checkForSelection(gameContainer, game);
+			
+			// Draw plane icon
+			this.planeIcon.draw((float) (((WindowManager) game).getPlaneProgress()
+					- 50), 50 + this.font.getHeight() + 105, 50, 50);
+		}
+		
+	
 	
 	// Accessors
 	/**
@@ -74,222 +304,6 @@ public class MainMenu extends GenericWindow {
 	public void setWaypointIcon(Image waypointIcon)
 	{
 		this.waypointIcon = waypointIcon;
-	}
-	
-	
-	// Other methods
-	/**
-	 * Monitors the mouse position and state
-	 * <p>
-	 * This causes options to be highlighted etc. when hovered over.
-	 * </p>
-	 * <p>
-	 * Also handles item selection (through clicking)
-	 * </p>
-	 * 
-	 * @param gameContainer	the game container holding this state
-	 * @param game			the game running this state
-	 */
-	private void checkForSelection(GameContainer gameContainer,
-			StateBasedGame game)
-	{
-		int x 					= gameContainer.getInput().getMouseX();
-		int y 					= gameContainer.getInput().getMouseY();
-		boolean clicked 		= gameContainer.getInput().isMousePressed(0);
-		
-		String startGameText 	= "Start Game";
-		String creditsText 		= "Credits";
-		String controlsText 	= "Controls";
-		String exitText 		= "Exit";
-		
-		int startGameWidth 		= this.font.getWidth(startGameText);
-		int creditsWidth 		= this.font.getWidth(creditsText);
-		int controlsWidth 		= this.font.getWidth(controlsText);
-		int exitWidth 			= this.font.getWidth(exitText);
-		
-		int textHeight 			= this.font.getHeight();
-		
-		Color startGameColor 	= Color.orange;
-		Color creditsColor 		= Color.orange;
-		Color controlsColor 	= Color.orange;
-		Color exitColor 		= Color.orange;
-		
-		// Start Game
-		if((x >= (gameContainer.getWidth() / 3) + 30 - 25)
-				&& (y >= (gameContainer.getHeight() / 3) + 50 - 25)
-				&& (x <= (gameContainer.getWidth() / 3) + 30
-				+ startGameWidth + 25)
-				&& (y <= (gameContainer.getHeight() / 3) + 50
-				+ textHeight + 25))
-		{
-			if(clicked)
-			{
-				game.enterState(WindowManager.LEVEL_SELECT_STATE);
-			}
-			else
-			{
-				startGameColor = Color.white;
-				this.waypointIcon.draw((gameContainer.getWidth() / 3)
-						+ 250, (gameContainer.getHeight() / 3) + 52);
-			}
-		}
-		else
-		{
-			startGameColor = Color.orange;
-		}
-		
-		this.drawShadowedText(this.font, (gameContainer.getWidth() / 3)
-				+ 30, (gameContainer.getHeight() / 3) + 50,
-				startGameText, startGameColor);
-		
-		// Credits
-		if((x >= (gameContainer.getWidth() / 3) + 30 - 25)
-				&& (y >= (gameContainer.getHeight() / 3) + 50 
-				+ (this.font.getHeight() * 4) - 25)
-				&& (x <= (gameContainer.getWidth() / 3) + 30
-				+ creditsWidth + 25)
-				&& (y <= (gameContainer.getHeight() / 3) + 50
-				+ (this.font.getHeight() * 4) + textHeight + 25))
-		{
-			if(clicked)
-			{
-				game.enterState(WindowManager.CREDITS_STATE);
-			}
-			else
-			{
-				creditsColor = Color.white;
-				
-				this.waypointIcon.draw((gameContainer.getWidth() / 3)
-						+ 30 + creditsWidth, (gameContainer.getHeight() / 3) + 54 + (this.font.getHeight() * 4));
-			}
-		}
-		else
-		{
-			creditsColor = Color.orange;
-		}
-		
-		this.drawShadowedText(this.font, (gameContainer.getWidth() / 3)
-				+ 30, (gameContainer.getHeight() / 3) + 50
-				+ (this.font.getHeight() * 4), creditsText, creditsColor);
-		
-		// Controls
-				if((x >= (gameContainer.getWidth() / 3) + 30 - 25)
-						&& (y >= (gameContainer.getHeight() / 3) + 150 
-						+ (this.font.getHeight() * 4))
-						&& (x <= (gameContainer.getWidth() / 3) + 30
-						+ creditsWidth + 25)
-						&& (y <= (gameContainer.getHeight() / 3) + 160
-						+ (this.font.getHeight() * 4) + textHeight + 25))
-				{
-					if(clicked)
-					{
-						game.enterState(WindowManager.CONTROLS_STATE);
-					}
-					else
-					{
-						controlsColor = Color.white;
-						this.waypointIcon.draw((gameContainer.getWidth() / 3)
-								+ 210, (gameContainer.getHeight() /3 + 230));
-					}
-				}
-				else
-				{
-					controlsColor = Color.orange;
-				}
-				
-		this.drawShadowedText(this.font, (gameContainer.getWidth() / 3)
-					+ 30, (gameContainer.getHeight() / 3) + 150
-					+ (this.font.getHeight() * 4), controlsText, controlsColor);
-				
-		
-		
-		// Exit
-		if((x >= (50 - 25))
-				&& (y >= (gameContainer.getHeight() - 50 - 25))
-				&& (x <= (50 - 25) + exitWidth + 25)
-				&& (y <= (gameContainer.getHeight() - 50 + textHeight + 25)))
-		{
-			if(clicked)
-			{
-				System.exit(0);
-			}
-			else
-			{
-				exitColor = Color.white;
-			}
-		}
-		else
-		{
-			exitColor = Color.orange;
-		}
-		
-		this.drawShadowedText(this.font, 50, gameContainer.getHeight()
-				- 50, exitText, exitColor);
-	}
-	
-	// Overrides
-	/**
-	 * Initialises the state
-	 * 
-	 * @param gameContainer	the game container holding this state
-	 * @param game			the game running this state
-	 */
-	@Override
-	public void init(GameContainer gameContainer,
-			StateBasedGame game) throws SlickException {
-		super.init(gameContainer, game);
-		
-		InputStream backgroundStream = this.getClass()
-				.getResourceAsStream(
-						"/resources/backgrounds/CloudBackground.png");
-		InputStream planeStream = this.getClass()
-				.getResourceAsStream("/resources/planes/TitlePlane.png");
-		InputStream waypointStream = this.getClass()
-				.getResourceAsStream("/resources/waypoints/WaypointRed.png");
-		
-		this.backgroundImage = new Image(backgroundStream,
-				"Background Image", false);
-		this.planeIcon = new Image(planeStream,
-				"Plane Icon", false);		
-		this.waypointIcon = new Image(waypointStream,
-				"Waypoint Icon", false);
-	}
-	
-	/**
-	 * Renders the state
-	 * 
-	 * @param gameContainer	the game container holding this state
-	 * @param game			the game running this state
-	 * @param graphics		the graphics container to display content in
-	 */
-	@Override
-	public void render(GameContainer gameContainer,
-			StateBasedGame game, Graphics graphics) throws SlickException {
-		super.render(gameContainer, game, graphics);
-		
-		// Draw background
-		this.backgroundImage.draw((float) (0 - ((WindowManager) game)
-				.getSkyProgress()), 0, gameContainer.getWidth() * 2,
-				gameContainer.getHeight());
-		
-		// Draw main title
-		int mainTitleXPos = (gameContainer.getWidth() / 2)
-				- (this.titleFont.getWidth("World of") / 2);
-		
-		int subtitleXPos = (gameContainer.getWidth() / 2)
-				- (this.titleFont.getWidth("Flighty") / 2);
-		
-		this.drawShadowedText(this.titleFont, mainTitleXPos, 50,
-				"World of", Color.orange);
-		this.drawShadowedText(this.titleFont, subtitleXPos, 125,
-				"Flighty", Color.orange);
-		
-		// Draw other text
-		this.checkForSelection(gameContainer, game);
-		
-		// Draw plane icon
-		this.planeIcon.draw((float) (((WindowManager) game).getPlaneProgress()
-				- 50), 50 + this.font.getHeight() + 105, 50, 50);
 	}
 	
 	/**
