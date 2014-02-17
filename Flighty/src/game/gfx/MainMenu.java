85a096214d09469585e14a4a25447b4968041c39
hk3380 <lp752@york.ac.uk>
2014-02-17 22:20:51 +0000

    i dont even remember what i changed

--- Flighty/src/game/gfx/MainMenu.java
+++ Flighty/src/game/gfx/MainMenu.java
@@ -7,8 +7,6 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.Music;
-
 import java.io.InputStream;
 
 
