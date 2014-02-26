fda6b40f2a8c7e2c8db1947fd6297b844f5669ae
Sam Heather <sam@heather.sh>
2014-02-26 17:56:47 +0000

    Efficiency improvements for Server by Jamaal

--- server/seprServer.py
+++ server/seprServer.py
@@ -10,11 +10,10 @@ from twisted.python import log
 log.startLogging(sys.stdout)
 log.msg("SLOG after start logging!")
 
-
 class MyChat(basic.LineReceiver):
 
 	__notInGame = []
-	__games = []
+	__opponent = None
 
 	def connectionMade(self):
 		print "SLOG"
@@ -23,35 +22,25 @@ class MyChat(basic.LineReceiver):
 		self.__notInGame.append(self)
 		print "SLOG 2"
 		while (len(self.__notInGame) >= 2):
-			self.__games.append(Game(self.__notInGame.pop(0),
-				self.__notInGame.pop(0)))
+			x = self.__notInGame.pop(0)
+			y = self.__notInGame.pop(0)
+			x.__opponent = y
+			y.__opponent = x
 		print "SLOG FINISH YAY"
 
 	def connectionLost(self, reason):
 		print "SLOG Lost a client!"
-		for g in self.__games:
-			if g.__clientA == self or g.__clientB == self:
-				self.__games.remove(g)
 		self.factory.clients.remove(self)
-
+		self.factory.clients.remove(self.__opponent)
 
 	def lineReceived(self, data):
-		for c in self.__games:
-			c.handleReceived(self, data)
+		if self.__opponent == None:
+			self.message("E0") # not in game
+			return
+		self.__opponent.message(data)
 
 	def message(self, message):
 		self.transport.write(message + '\n')
-
-class Game():
-	def __init__(self, clientA, clientB):
-		self.__clientA = clientA
-		self.__clientB = clientB
-	def handleReceived(self, client, data):
-		if (client == self.__clientA):
-			self.__clientB.message(data)
-		elif (client == self.__clientB):
-			self.__clientA.message(data)
-
 
 from twisted.internet import protocol
 from twisted.application import service, internet
