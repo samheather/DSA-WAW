f36a7be745ddc5c17b26497cec448fa6f202e935
Sam Heather <sam@heather.sh>
2014-02-28 14:54:12 +0000

    Fixed bytebuffer random generator

--- Flighty/src/game/gfx/WindowManager.java
+++ Flighty/src/game/gfx/WindowManager.java
@@ -75,19 +75,19 @@ public class WindowManager extends StateBasedGame {
 		//////////// DO NOT DELETE THIS - SAM'S TEST CODE ////////////
 		//////////////////////////////////////////////////////////////
 		
-		Socket s = new Socket("localhost", 1025);
-		byte[] received = new byte[1024];
-		InputStream is = s.getInputStream();
-		OutputStream os = s.getOutputStream();
-		if (is.read(received) > 1023) {
-			System.out.println("Byte array may have overflowed - max size=1024 - increase this!");
-		}
-		String toPrint = new String(received);
-		System.out.println(toPrint);
-		os.write("Hello from java!\n".getBytes()); // Don't forget the /n
-		is.close();
-		os.close();
-		s.close();
+//		Socket s = new Socket("teaching0.york.ac.uk", 1025);
+//		byte[] received = new byte[1024];
+//		InputStream is = s.getInputStream();
+//		OutputStream os = s.getOutputStream();
+//		if (is.read(received) > 1023) {
+//			System.out.println("Byte array may have overflowed - max size=1024 - increase this!");
+//		}
+//		String toPrint = new String(received);
+//		System.out.println(toPrint);
+//		os.write("Hello from java!\n".getBytes()); // Don't forget the /n
+//		is.close();
+//		os.close();
+//		s.close();
 
 		try {
 			// Set up app for game
