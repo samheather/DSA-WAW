92808e003921793949874e747242a93634fa4702
tapuu <mjm540@york.ac.uk>
2014-04-29 12:26:29 +0100

    Overridden hashcode and equals methods to use the unique network id

--- Flighty/src/game/struct/MultiplayerGame.java
+++ Flighty/src/game/struct/MultiplayerGame.java
@@ -82,8 +82,7 @@ public class MultiplayerGame extends Game {
 					ListIterator<MultiplayerPlane> i = multiplayerPlanes.listIterator();
 					while (i.hasNext()) {
 						Plane p2 = i.next();
-						if (p2.getUniqueNetworkObjectID() == p
-								.getUniqueNetworkObjectID()) {
+						if (p2.equals(p)) {
 							//p.ownedByCurrentPlayer = false;
 							if (p.deleted())
 								i.remove();
