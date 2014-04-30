e41ba974c20a205388fa98197dc2762103c36db4
Sam Heather <sam@heather.sh>
2014-04-30 17:47:32 +0100

    Fixed score that changed after game ended

--- Flighty/src/game/gfx/GameWindow.java
+++ Flighty/src/game/gfx/GameWindow.java
@@ -46,6 +46,8 @@ public class GameWindow extends BasicGameState {
 	/** The height the game is displayed at */
 	public int windowHeight;
 	
+	private int endScore;
+	
 	/** Lowest leaderboard score*/
 	
 	private long lowScore;
@@ -1007,7 +1009,7 @@ public class GameWindow extends BasicGameState {
 
 				// Draw the game over text
 				endFont.drawString(300, 200, "That didn't end well");
-				endFont.drawString(400, 260, "Score: "+ (int) this.currentGame.getScore().getScore());
+				endFont.drawString(400, 260, "Score: "+ this.endScore);
 
 				int textHeight = this.font.getHeight();
 
@@ -1089,6 +1091,7 @@ public class GameWindow extends BasicGameState {
 				else {
 					// Stop the timer
 					this.endTime = this.time;
+					this.endScore = currentGame.getScore().getScore();
 					// End the game
 					this.currentGame.setEnding(true);
 				}
