e5920de49dd1560a264d7f927372ce9d314142f5
Sam Heather <sam@heather.sh>
2014-05-05 11:04:16 +0100

    Massive commenting, relabeling javadocing and manual re-formatting of game.vision

--- Flighty/src/game/vision/LocateFace.java
+++ Flighty/src/game/vision/LocateFace.java
@@ -15,29 +15,60 @@ import java.util.Arrays;
 /**
  * Class for providing a constant stream of positions of a users face with the
  * option for advanced error detection of outliers and error correction.
- * @author Samuel Heather (sam@heather.sh)
- * © 2014 Samuel Heather - All Rights Reserved.
+ * 
+ * @author Samuel Heather (sam@heather.sh) © 2014 Samuel Heather - All Rights
+ *         Reserved.
  */
 public class LocateFace {
-	
+
 	private VideoCapture vc;
 	private CascadeClassifier cascaseFaceDetectorProfile;
 	private MatOfRect faces;
+
 	/**
 	 * Rect used to represent the first face found, that is returned when a face
 	 * is requested via getLocation().
 	 */
 	private Rect mainFace;
+
+	/** Minimum size that will be recognised as a face */
 	private Size minSize;
+
+	/** Maximum size that will be recognised as a face */
 	private Size maxSize;
+
+	/** Width of the camera image */
 	private int imgWidth;
+
+	/** Height of the camera image */
 	private int imgHeight;
-	
+
+	/**
+	 * Holds the last five detected X positions of the face for error
+	 * detection/correction.
+	 */
 	private float lastFiveX[] = new float[5];
+
+	/**
+	 * Holds the last five detected Y positions of the face for error
+	 * detection/correction.
+	 */
 	private float lastFiveY[] = new float[5];
+
+	/**
+	 * Holds the last five detected measurements of distance between the face
+	 * and camera for error detection/correction.
+	 */
 	private float lastFiveDistance[] = new float[5];
+
+	/** Initial value for lastFive arrays */
 	private float initialValue = 9999.9f;
-	
+
+	/**
+	 * Counter for concurrent outliers or errors. When a pre-determined val is
+	 * reached, it is assumed the face position changed significantly and all
+	 * past position history will be reset.
+	 */
 	private int outliersOrErrors = 0;
 
 	/**
@@ -47,60 +78,63 @@ public class LocateFace {
 	public LocateFace() {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		vc = new VideoCapture(0);
-		
+
 		// Load the face detection haarcascade file in
 		File cascadeFile = new File("res/haarcascade_frontalface_default.xml");
 		cascaseFaceDetectorProfile = new CascadeClassifier(
 				cascadeFile.getAbsolutePath());
-		
-		// Set min and max sizes of face to detect.  May vary for System.
-		minSize = new Size(125,125);
-		maxSize = new Size(700,700);
-		
-		// Initialise the lastFive Arrays to a value that can never be generated from Face Tracking
+
+		// Set min and max sizes of face to detect. May vary for System.
+		minSize = new Size(125, 125);
+		maxSize = new Size(700, 700);
+
+		// Initialise the lastFive Arrays to a value that can never be generated
+		// from Face Tracking
 		Arrays.fill(lastFiveX, initialValue);
 		Arrays.fill(lastFiveY, initialValue);
 		Arrays.fill(lastFiveDistance, initialValue);
 	}
-	
+
 	/**
 	 * Called whenever a new face location is requested by the Vision class.
+	 * 
 	 * @return boolean successful
 	 */
 	public boolean updateFacePosition() {
 		// Read image from camera into matrix.
 		Mat img = new Mat();
-		if(!vc.read(img)) {
-			System.out.println("Error capturing image from CameraCapture - no camera detected.");
+		if (!vc.read(img)) {
+			System.out
+					.println("Error capturing image from CameraCapture - no camera detected.");
 			return false;
 		}
 		imgWidth = img.width();
 		imgHeight = img.height();
-		
+
 		// TODO(samheather) Convert image to gray+make smaller for ++er fps?
 		faces = new MatOfRect();
 		cascaseFaceDetectorProfile.detectMultiScale(img, faces, 1.05, 6, 0,
 				minSize, maxSize);
-		
+
 		if (!faces.empty()) {
 			mainFace = faces.toArray()[0];
 			System.out.println(mainFace);
-			
+
 			// Add to Average Arrays
 			addToLastFive(getImmediateDistance(false), lastFiveDistance);
 			addToLastFive(getImmediateHorizontalAngle(false), lastFiveX);
 			addToLastFive(getImmediateVerticalAngle(false), lastFiveY);
 			return true;
-		}
-		else {
+		} else {
 			outliersOrErrors++;
 			return false;
 		}
 	}
-	
+
 	/**
-	 * Adds a new value to an array of the previous 5 values for a parameter, after
-	 * shuffling the previous elements to accommodate this.
+	 * Adds a new value to an array of the previous 5 values for a parameter,
+	 * after shuffling the previous elements to accommodate this.
+	 * 
 	 * @param newValue
 	 * @param lastFive
 	 */
@@ -112,22 +146,25 @@ public class LocateFace {
 			Arrays.fill(lastFive, 9999.9f);
 			System.out.println("\n\n\n\n\nResetting - Too many outliers\n\n");
 		}
-		// Check not an outlier - if last face position more than 10% different to previous average.
-		if (Math.abs(meanOfLastFive(lastFive)-newValue) > imgWidth/10 && meanOfLastFive(lastFive) != 0) {
+		// Check not an outlier - if last face position more than 10% different
+		// to previous average.
+		if (Math.abs(meanOfLastFive(lastFive) - newValue) > imgWidth / 10
+				&& meanOfLastFive(lastFive) != 0) {
 			System.out.println("Outlier detected");
 			outliersOrErrors += 10;
 			return;
 		}
 		outliersOrErrors = 0;
 		for (int i = 3; i >= 0; i--) {
-			lastFive[i+1] = lastFive[i];
+			lastFive[i + 1] = lastFive[i];
 		}
 		lastFive[0] = newValue;
 	}
-	
+
 	/**
 	 * Returns the mean of the 5 objects in the Last Datapoints arrays, ignoring
 	 * (and accommodating for) unfilled spaces in the array.
+	 * 
 	 * @param lastFive
 	 * @return
 	 */
@@ -144,42 +181,45 @@ public class LocateFace {
 			System.out.println("Divisor is 0, returning 0");
 			return 0f;
 		}
-		return count/divisor;
+		return count / divisor;
 	}
-	
+
 	/**
 	 * Returns a representation of how far the face is from the camera.
+	 * 
 	 * @return float distance
 	 */
 	public float getImmediateDistance(boolean errorCorrectUsingMean) {
 		if (errorCorrectUsingMean) {
 			return meanOfLastFive(lastFiveDistance);
 		}
-		return (float)mainFace.height;
+		return (float) mainFace.height;
 	}
-	
+
 	/**
 	 * Returns a representation of the horizontal angle between the face and the
 	 * line perpendicular to the camera.
+	 * 
 	 * @return float horizontalAngle
 	 */
 	public float getImmediateHorizontalAngle(boolean errorCorrectUsingMean) {
 		if (errorCorrectUsingMean) {
 			return meanOfLastFive(lastFiveX);
 		}
-		return mainFace.x+(mainFace.width/2)-(imgWidth/2);
+		return mainFace.x + (mainFace.width / 2) - (imgWidth / 2);
 	}
-	
+
 	/**
 	 * Returns a representation of the vertical angle between the face and the
 	 * line perpendicular to the camera.
+	 * 
 	 * @return float verticalAngle
 	 */
 	public float getImmediateVerticalAngle(boolean errorCorrectUsingMean) {
 		if (errorCorrectUsingMean) {
 			return meanOfLastFive(lastFiveY);
 		}
-		return mainFace.y+(mainFace.height/2)-(imgHeight/2);
+		return mainFace.y + (mainFace.height / 2) - (imgHeight / 2);
 	}
 
 }
