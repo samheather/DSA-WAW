5a763ea21569b556ff0fd0ac16683c886367bc18
Sam Heather <sam@heather.sh>
2014-05-06 21:28:44 +0100

    Loads more Javadoc

--- Flighty/src/game/struct/Point.java
+++ Flighty/src/game/struct/Point.java
@@ -1,45 +1,82 @@
 package game.struct;
 
+/**
+ * Class references any generic point.
+ * @author Samuel
+ *
+ */
 public class Point {
 
+	/** X position of point */
 	protected double x = 0.0;
+	
+	/** Y position of point */
 	protected double y = 0.0;
+	
+	/** String reference to the point */
 	protected String pointRef = "";
 
-	// CONSTRUCTORS
-
-	// Point Constructor taking two doubles for X then Y coordinates.
+	/**
+	 * Point Constructor taking two doubles for X then Y coordinates.
+	 * @param xcoord
+	 * @param ycoord
+	 */
 	public Point(double xcoord, double ycoord) {
 		x = xcoord;
 		y = ycoord;
 	}
 
+	/** Empty constructor*/
 	public Point() {
 
 	}
 
-	// SETTERS AND GETTERS
+	// Accessors and Mutators
 
+	/**
+	 * Returns X position of point
+	 * @return
+	 */
 	public double getX() {
 		return this.x;
 	}
 
+	/**
+	 * Returns Y position of point
+	 * @return
+	 */
 	public double getY() {
 		return this.y;
 	}
 
+	/**
+	 * Sets X position of point.
+	 * @param newX
+	 */
 	public void setX(double newX) {
 		this.x = newX;
 	}
 
+	/**
+	 * Sets Y position of point.
+	 * @param newY
+	 */
 	public void setY(double newY) {
 		this.y = newY;
 	}
 
+	/**
+	 * Get position reference
+	 * @return
+	 */
 	public String getPointRef() {
 		return this.pointRef;
 	}
 
+	/**
+	 * Sets position ref to input String
+	 * @param pointRef
+	 */
 	public void setPointRef(String pointRef) {
 		this.pointRef = pointRef;
 	}
